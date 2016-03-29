ea9f7cd9de9eb516091b2eecec7cfdef952b6ce1
Nathan Lamley <fearitself4087@hotmail.com>
2016-03-29 12:57:47 -0400

    Commit

---
+++ FinalProject/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStartUp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmStartUp))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblStartUp = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.lblAdd = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Controls.Add(Me.btnStart)
+        Me.Panel1.Controls.Add(Me.lblAdd)
+        Me.Panel1.Controls.Add(Me.txtUserName)
+        Me.Panel1.Controls.Add(Me.lblStartUp)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(859, 461)
+        Me.Panel1.TabIndex = 0
+        '
+        'lblStartUp
+        '
+        Me.lblStartUp.AutoSize = True
+        Me.lblStartUp.BackColor = System.Drawing.Color.Transparent
+        Me.lblStartUp.Font = New System.Drawing.Font("Segoe Print", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStartUp.ForeColor = System.Drawing.Color.Maroon
+        Me.lblStartUp.Location = New System.Drawing.Point(313, 15)
+        Me.lblStartUp.Name = "lblStartUp"
+        Me.lblStartUp.Size = New System.Drawing.Size(216, 47)
+        Me.lblStartUp.TabIndex = 0
+        Me.lblStartUp.Text = "My Corkboard"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.BackColor = System.Drawing.Color.White
+        Me.txtUserName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtUserName.Location = New System.Drawing.Point(309, 149)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(194, 20)
+        Me.txtUserName.TabIndex = 1
+        '
+        'lblAdd
+        '
+        Me.lblAdd.AutoSize = True
+        Me.lblAdd.BackColor = System.Drawing.Color.Transparent
+        Me.lblAdd.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAdd.ForeColor = System.Drawing.Color.Maroon
+        Me.lblAdd.Location = New System.Drawing.Point(291, 124)
+        Me.lblAdd.Name = "lblAdd"
+        Me.lblAdd.Size = New System.Drawing.Size(224, 15)
+        Me.lblAdd.TabIndex = 2
+        Me.lblAdd.Text = "Enter your name to get started"
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.Transparent
+        Me.btnStart.Enabled = False
+        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnStart.Font = New System.Drawing.Font("Lucida Sans", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.Color.Maroon
+        Me.btnStart.Location = New System.Drawing.Point(351, 175)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(108, 23)
+        Me.btnStart.TabIndex = 3
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'frmStartUp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(859, 461)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.Name = "frmStartUp"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "StartUp"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblStartUp As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lblAdd As Label
+    Friend WithEvents txtUserName As TextBox
+End Class
---
+++ FinalProject/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmStartUp
+    Private Sub txtUserName_TextChanged(sender As Object, e As EventArgs) Handles txtUserName.TextChanged
+        If IsNumeric(txtUserName.Text) Then
+            btnStart.Enabled = False
+        ElseIf txtUserName.Text.Length = 0
+            btnStart.Enabled = False
+        Else
+            btnStart.Enabled = True
+        End If
+    End Sub
+End Class
